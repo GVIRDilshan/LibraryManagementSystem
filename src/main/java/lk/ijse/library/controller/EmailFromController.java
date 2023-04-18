4396cb3a7bf5499ef44e0df2f293a87833cf10a3
GVIRDilshan <ishanravidu750@gmail.com>
2023-04-18 16:11:08 +0530

    new codes

--- src/main/java/lk/ijse/library/controller/EmailFromController.java
+++ src/main/java/lk/ijse/library/controller/EmailFromController.java
@@ -1,4 +1,26 @@
 package lk.ijse.library.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class EmailFromController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXTextArea txtWriteEmail;
+
+    @FXML
+    private JFXComboBox<?> cmdSelectEmail;
+
+    public void OnSelectEmail(ActionEvent actionEvent) {
+
+    }
+
+    public void OnSend(ActionEvent actionEvent) {
+
+    }
 }
