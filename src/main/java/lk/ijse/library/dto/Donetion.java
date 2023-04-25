178770aa519c0324fb51be12a801f117882ac0ad
GVIRDilshan <ishanravidu750@gmail.com>
2023-04-25 08:52:20 +0530

    new changes

--- src/main/java/lk/ijse/library/dto/Donetion.java
+++ src/main/java/lk/ijse/library/dto/Donetion.java
@@ -1,4 +1,17 @@
 package lk.ijse.library.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Donetion {
+    private String donetionId;
+    private String review;
+    private int Ammount;
+    private String donetionName;
+    private String ExibitionId;
+
 }
